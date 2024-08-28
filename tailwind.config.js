3836941eeec4df3760582f786ac566edc6ce1ca9
GentooWizard <demidfr@proton.me>
2024-08-28 20:26:09 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+		colors: {
+			'ebdbb': "#ebdbb2",
+			'28': "#282828",
+		},
+	}
+  },
+  plugins: [],
+}
